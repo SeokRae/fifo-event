21f93eba20a234143d9d99facb6042dd4d946027
seokrae <kslbsh@gmail.com>
2023-07-13 21:42:38 +0900

    Initial commit

---
+++ src/main/java/com/example/fifoevent/FifoEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.fifoevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifoEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifoEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fifoevent/FifoEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fifoevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifoEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
