d7aa8b57532da6c96c117ed8407ffb1a63fa15bd
seokrae <kslbsh@gmail.com>
2023-07-14 00:03:05 +0900

    - [x] 레디스 -> 카프카 -> db 저장 실시간 처리 불가 테스트 확인

---
+++ fifo-consumer/src/main/java/com/example/fifoconsumer/domain/Coupon.java
@@ -0,0 +1,26 @@
+package com.example.fifoconsumer.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    public Coupon(Long userId) {
+        this.userId = userId;
+    }
+}
