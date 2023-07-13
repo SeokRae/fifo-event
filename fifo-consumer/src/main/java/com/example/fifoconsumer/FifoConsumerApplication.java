553538016bbe230fd123cde77f471d1955ade77f
seokrae <kslbsh@gmail.com>
2023-07-13 23:44:43 +0900

    - [x] 카프카 consumer module 추가

---
+++ fifo-consumer/src/main/java/com/example/fifoconsumer/FifoConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fifoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifoConsumerApplication.class, args);
+	}
+
+}
---
+++ fifo-consumer/src/test/java/com/example/fifoconsumer/FifoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fifoconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifoConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
